528ebc126e3f2576b33a25d6135bab21c2813279
Scott Simonson <ssimonson@practicevelocity.com>
2017-12-12 11:42:11 -0600

    Added IndentificationReturnModel so we can map data before sending it off.

---
+++ PV_Doc_Template/IndentificationReturnModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV_Doc_Template
+{
+    public class IndentificationReturnModel
+    {
+        private string firstName { get; set; }
+        private string lastName { get; set; }
+        private string middleName { get; set; }
+        private string sex { get; set; }
+        private DateTime dateofBirth { get; set; }
+        private string address1 { get; set; }
+        private string address2 { get; set; }
+        private string city { get; set; }
+        private string state { get; set; }
+        private string zip { get; set; }
+        private string indentificationImage { get; set; }
+    }
+}
