85370d6b734268e000034262b3c0f1ae55a28cd0
PV\jjanicki <jajanicki@gmail.com>
2017-12-12 14:44:52 -0600

    Adding OCR raw data model

---
+++ PV_Doc_Template/IndentificationReturnModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV_Doc_Template
+{
+    public class IdentificationReturnModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string middleName { get; set; }
+        public string sex { get; set; }
+        public DateTime dateofBirth { get; set; }
+        public string address1 { get; set; }
+        public string address2 { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string indentificationImage { get; set; }
+    }
+}
---
+++ PV_Doc_Template/OCRRawDataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV_Doc_Template
+{
+    public class OCRRawDataModel
+    {
+        public List<RawDataItem> DataList { get; set; } 
+
+        public class RawDataItem 
+        {
+            public string Value { get; set; }
+            public int LineIndex { get; set; }
+        }
+    }
+}
