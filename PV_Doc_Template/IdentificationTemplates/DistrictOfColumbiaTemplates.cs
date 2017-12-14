ff2c75fdaa7cc2a1d71b73f84d41d1afc219dd20
Scott Simonson <ssimonson@practicevelocity.com>
2017-12-14 08:58:14 -0600

    Correcting the issue where the templates didn't get added to the cproj file. I also corrected the issue with the districtOfColumbiaTemplates class.

--- PV_Doc_Template/IdentificationTemplates/DistrictOfColumbiaTemplates.cs
+++ PV_Doc_Template/IdentificationTemplates/DistrictOfColumbiaTemplates.cs
@@ -7,65 +7,65 @@ using PV_Doc_Template.HelperClasses;
 
 namespace PV_Doc_Template.IdentificationTemplates
 {
-    public lass DistrictOfColumbiaTemplates
+    public class DistrictOfColumbiaTemplates
     {
-    public IdentificationReturnModel MapDataToTemplate(List<OCRRawDataModel.RawDataItem> data)
-    {
-        var dataHelper = new DataHelpers();
-        var model = new IdentificationReturnModel();
-        StringBuilder addressStringBuilder = new StringBuilder();
-        StringBuilder lastNameStringBuilder = new StringBuilder();
-
-        var containsStuff = data.Any(v => v.Value.Contains("DRIVER")) && data.Any(v => v.Value.Contains("LICENSE"));
-        if (containsStuff)
+        public IdentificationReturnModel MapDataToTemplate(List<OCRRawDataModel.RawDataItem> data)
         {
-            foreach (var item in data)
+            var dataHelper = new DataHelpers();
+            var model = new IdentificationReturnModel();
+            StringBuilder addressStringBuilder = new StringBuilder();
+            StringBuilder lastNameStringBuilder = new StringBuilder();
+
+            var containsStuff = data.Any(v => v.Value.Contains("DRIVER")) && data.Any(v => v.Value.Contains("LICENSE"));
+            if (containsStuff)
             {
-                //Get Birthday
-                var birthDate = dataHelper.GetBirthday(item);
-                if (birthDate != DateTime.MinValue)
+                foreach (var item in data)
                 {
-                    model.dateofBirth = birthDate;
-                }
-                //Get state
-                var state = dataHelper.GetState(item);
-                if (!string.IsNullOrWhiteSpace(state))
-                {
-                    model.state = item.Value;
-                    var zipIndex = data.FindIndex(a => a.Value.Equals(item.Value)) + 1;
-                    model.zip = data[zipIndex].Value;
-                }
-                //Get city
-
-                //Build Address
-
-                if (item.LineIndex == 6)
-                {
-                    addressStringBuilder.Append(item.Value.Trim()).Append(" ");
-                    model.address1 = addressStringBuilder.ToString().TrimEnd();
-                }
-
-                //Get LastName using the line index, which should always be the 4th line
-                if (item.LineIndex == 4)
-                {
-                    lastNameStringBuilder.Append(item.Value.Trim()).Append(" ");
-                    model.lastName = lastNameStringBuilder.ToString().TrimEnd();
-                }
-
-                //Get FirstName and middle name
-                if (item.LineIndex == 5)
-                {
-                    model.firstName = string.IsNullOrWhiteSpace(model.firstName) ? item.Value : model.firstName;
-                    //Assuming the the middle name comes after the first name, this may not be true, but we don't have a better way to get it yet.
-                    if (!string.IsNullOrWhiteSpace(model.firstName))
+                    //Get Birthday
+                    var birthDate = dataHelper.GetBirthday(item);
+                    if (birthDate != DateTime.MinValue)
                     {
-                        var middleNameIndex = data.FindIndex(a => a.Value.Equals(model.firstName)) + 1;
-                        model.middleName = data[middleNameIndex].Value;
+                        model.dateofBirth = birthDate;
+                    }
+                    //Get state
+                    var state = dataHelper.GetState(item);
+                    if (!string.IsNullOrWhiteSpace(state))
+                    {
+                        model.state = item.Value;
+                        var zipIndex = data.FindIndex(a => a.Value.Equals(item.Value)) + 1;
+                        model.zip = data[zipIndex].Value;
+                    }
+                    //Get city
+
+                    //Build Address
+
+                    if (item.LineIndex == 6)
+                    {
+                        addressStringBuilder.Append(item.Value.Trim()).Append(" ");
+                        model.address1 = addressStringBuilder.ToString().TrimEnd();
+                    }
+
+                    //Get LastName using the line index, which should always be the 4th line
+                    if (item.LineIndex == 4)
+                    {
+                        lastNameStringBuilder.Append(item.Value.Trim()).Append(" ");
+                        model.lastName = lastNameStringBuilder.ToString().TrimEnd();
+                    }
+
+                    //Get FirstName and middle name
+                    if (item.LineIndex == 5)
+                    {
+                        model.firstName = string.IsNullOrWhiteSpace(model.firstName) ? item.Value : model.firstName;
+                        //Assuming the the middle name comes after the first name, this may not be true, but we don't have a better way to get it yet.
+                        if (!string.IsNullOrWhiteSpace(model.firstName))
+                        {
+                            var middleNameIndex = data.FindIndex(a => a.Value.Equals(model.firstName)) + 1;
+                            model.middleName = data[middleNameIndex].Value;
+                        }
                     }
                 }
             }
+            return model;
         }
-        return model;
     }
-}
 }
